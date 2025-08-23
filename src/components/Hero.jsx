9457465d2a152926f9873108055da6ef3a1ffd50
Jay09990 <jdudhrejiya0990@gmail.com>
2025-08-23 21:32:40 +0530

    improvised

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,24 @@
 import { useGSAP } from "@gsap/react";
 import { SplitText } from "gsap/all";
 import gsap from "gsap";
-import React from "react";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", { type: "chars,words" });
+
     const paragraphSplit = new SplitText(".subtitle", { type: "lines" });
 
     heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
       yPercent: 100,
-      duration: 1.6,
+      duration: 1.8,
       ease: "expo.out",
       stagger: 0.06,
     });
@@ -26,23 +32,43 @@ const Hero = () => {
       delay: 0.7,
     });
 
-    
-     gsap
-       .timeline({
-         scrollTrigger: {
-           trigger: "#hero",
-           start: "top top",
-           end: "bottom top",
-           scrub: true,
-         },
-       })
-       .to(".right-leaf", { y: 200 }, 0)
-       .to(".left-leaf", { y: -200 }, 0);
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom top",
+          scrub: true,
+        },
+      })
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    let tl = gsap.timeline({  
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
+
   return (
     <>
       <section id="hero" className="noisy">
         <h1 className="title">MOJITO</h1>
+        
         <img
           src="/images/hero-left-leaf.png"
           alt="left-leaf"
@@ -69,11 +95,22 @@ const Hero = () => {
                 creative flair, and timeless recipes - designed to delight your
                 senses.
               </p>
-              <a href="#cocktails" className="subtitle" >View Cocktails</a>
+              <a href="#cocktails" className="subtitle">
+                View Cocktails
+              </a>
             </div>
           </div>
         </div>
       </section>
+      <div className="video" absolute inset-0>
+        <video
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+          ref={videoRef}
+        ></video>
+      </div>
     </>
   );
 };

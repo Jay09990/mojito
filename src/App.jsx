cc5632eb462aabdeaaeb89d99bd219e2dbf16a52
Jay09990 <jdudhrejiya0990@gmail.com>
2025-08-24 23:51:47 +0530

    some changes in path

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import gsap from "gsap";
 
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
-import Cocktails from "./components/cocktails.jsx";
+import cocktails from "./components/";
 import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
 import Menu from "./components/Menu.jsx";
